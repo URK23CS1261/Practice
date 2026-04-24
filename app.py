73a4dbc87f92c859f523538bd7bdb6d69b234bc4
URK23CS1261 <153481394+URK23CS1261@users.noreply.github.com>
2026-04-24 16:52:49 +0000

    Modified

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 def add(a, b):
     return a + b
-def greet(txt):
-    return f"Hello, {txt}!"
+def greet(name):
+    return f"Hello, {name}!"
--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from app import add, greet
 
 def test_add():
-    assert add(1, 4) == 5
+    assert add(4, 5) == 9
 def test_greet():
     assert greet("Alex") == "Hello, Alex!"
