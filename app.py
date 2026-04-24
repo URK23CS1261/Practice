45d336f93b9b0413a54e161e15bc5c3e239f6f27
URK23CS1261 <153481394+URK23CS1261@users.noreply.github.com>
2026-04-24 17:18:50 +0000

    Docker

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-def add(a, b):
-    return a + b
-def greet(name):
-    return f"Hello, {name}!"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Fask app running in docker!<h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++
@@ -1,6 +0,0 @@
-from app import add, greet
-
-def test_add():
-    assert add(4, 5) == 9
-def test_greet():
-    assert greet("Alex") == "Hello, Alex!"
