d20651ab0395cff022890786efaf73e3be902b48
URK23CS1261 <153481394+URK23CS1261@users.noreply.github.com>
2026-04-24 18:36:52 +0000

    Github actions and docker hub push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Fask app running in docker!<h1>"
+    return "<h1>Flask App is working</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
